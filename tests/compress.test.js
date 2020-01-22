d488d1da2b2cd05a93de4571973a59174b1f3dae
Sem Postma <lestergallagher18@gmail.copm>
2020-01-22 09:49:46 +0100

    various

--- tests/compress.test.js
+++ tests/compress.test.js
@@ -1,11 +1,10 @@
 const { compress, decompress } = require('../index');
-const schema = require('./testschema.json');
-const json = require('./testjson.json');
+var jsonSchemaGenerator = require('json-schema-generator')
 
 test('compresses json to a smaller footprint', () => {
+    const schema = require('./testschema.json');
+    const json = require('./testjson.json');
     const result = compress(json, schema);
-
-    console.log(JSON.stringify(result, null, 4));
 
     const before = JSON.stringify(json).length;
     const after = JSON.stringify(result).length;
@@ -14,7 +13,29 @@ test('compresses json to a smaller footprint', () => {
     console.log('saved', 100 - after / before * 100 + '%');
 });
 
+// test('compresses a medium size json payload to a smaller footprint', () => {
+//     const json = require('./largejson.json');
+//     const schema = jsonSchemaGenerator(json)
+//     const result = compress(json, schema);
+
+//     const before = JSON.stringify(json).length;
+//     const after = JSON.stringify(result).length;
+
+//     expect(after).toBeLessThan(before);
+//     console.log('saved', 100 - after / before * 100 + '%');
+// });
+
+// test('compress and decompress string for a medium size json payload and get the same output', () => {
+//     const json = require('./largejson.json');
+//     const schema = jsonSchemaGenerator(json)
+//     const compressed = compress(json, schema);
+//     const decompressed = decompress(compressed, schema);
+//     expect(json).toEqual(decompressed);
+// });
+
 test('compress and decompress string and get the same output', () => {
+    const schema = require('./testschema.json');
+    const json = require('./testjson.json');
     const compressed = compress(json, schema);
     const decompressed = decompress(compressed, schema);
     expect(json).toEqual(decompressed);
